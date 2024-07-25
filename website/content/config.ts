47b0ca523cf77ce87a77af64c9e0e681d00ade25
Lingkai.Xu <3234555+lingkaix@users.noreply.github.com>
2024-07-25 21:30:17 +1000

    chore: move website data

---
+++ website/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+// REF: https://docs.astro.build/en/guides/content-collections/
+const demo = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { demo };
